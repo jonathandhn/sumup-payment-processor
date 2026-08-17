d334224323d399ae9c680b1404d3ce834123d38e
Jonathan Dahan <jonathan@dhn.one>
2026-08-17 05:39:59 +0200

    fix(afform): make mount container always present in DOM with ng-show and robust SDK loader

--- ang/afSumUp/sumUpEmbeddedCheckout.component.js
+++ ang/afSumUp/sumUpEmbeddedCheckout.component.js
@@ -12,15 +12,30 @@
       this.$onDestroy = () => this.getFormElement().off('crmFormSuccess', listener);
       this.getFormElement = () => $element.closest('af-form');
 
+      function loadSdkScript(url) {
+        return new Promise(function (resolve, reject) {
+          if (document.querySelector('script[src="' + url + '"]')) {
+            resolve();
+            return;
+          }
+          var script = document.createElement('script');
+          script.src = url;
+          script.async = true;
+          script.onload = () => resolve();
+          script.onerror = () => reject(new Error('Failed to load ' + url));
+          document.head.appendChild(script);
+        });
+      }
+
       function ensureCheckoutSdk() {
         var tasks = [];
-        if (!window.SumUpCard && typeof CRM.loadScript === 'function') {
-          tasks.push(CRM.loadScript('https://gateway.sumup.com/gateway/ecom/card/v2/sdk.js'));
+        if (!window.SumUpCard) {
+          tasks.push(loadSdkScript('https://gateway.sumup.com/gateway/ecom/card/v2/sdk.js'));
         }
         if (!window.CiviSumUpCheckout) {
           var url = CRM.resources ? CRM.resources.getUrl('sumup-payment-processor', 'js/checkout.js') : null;
-          if (url && typeof CRM.loadScript === 'function') {
-            tasks.push(CRM.loadScript(url));
+          if (url) {
+            tasks.push(loadSdkScript(url));
           }
         }
         return Promise.all(tasks);
@@ -36,6 +51,7 @@
         var response = data.submissionResponse;
         var checkout = response && response[0] && response[0].sumup_embedded_checkout;
         if (!checkout) {
+          console.error('Missing sumup_embedded_checkout in submission response:', response);
           this.error = ts('Unable to start the secure payment form. Please try again.');
           return;
         }
@@ -51,25 +67,25 @@
         this.checkout = checkout;
         this.error = '';
 
-        $scope.$applyAsync(() => {
+        ensureCheckoutSdk().then(() => {
+          if (!window.CiviSumUpCheckout || typeof window.CiviSumUpCheckout.mount !== 'function') {
+            throw new Error('SumUp checkout SDK unavailable');
+          }
           var container = $element[0].querySelector('#sumup-afform-mount-target');
           if (!container) {
-            return;
+            throw new Error('Missing mount container #sumup-afform-mount-target');
           }
           container.innerHTML = '';
-          ensureCheckoutSdk().then(() => {
-            if (!window.CiviSumUpCheckout || typeof window.CiviSumUpCheckout.mount !== 'function') {
-              throw new Error('SumUp checkout SDK unavailable');
-            }
-            return window.CiviSumUpCheckout.mount(container, checkout);
-          }).then(() => {
-            if (typeof container.scrollIntoView === 'function') {
-              container.scrollIntoView({behavior: 'smooth', block: 'nearest'});
-            }
-          }).catch(() => {
-            this.error = ts('Unable to start the secure payment form. Please try again.');
-            $scope.$applyAsync();
-          });
+          return window.CiviSumUpCheckout.mount(container, checkout);
+        }).then(() => {
+          var container = $element[0].querySelector('#sumup-afform-mount-target');
+          if (container && typeof container.scrollIntoView === 'function') {
+            container.scrollIntoView({behavior: 'smooth', block: 'nearest'});
+          }
+        }).catch((err) => {
+          console.error('SumUp Afform Mount Failure:', err);
+          this.error = ts('Unable to start the secure payment form. Please try again.');
+          $scope.$applyAsync();
         });
       };
 
